1f0cc265d4f27735a21014b9d2b1acc620b897ee
Sean Martz <martzy13@gmail.com>
2016-08-22 22:33:41 -0400

    Stub out if logic; add q's

--- sketch_aug19a/sketch_aug19a.ino
+++ sketch_aug19a/sketch_aug19a.ino
@@ -20,19 +20,32 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   // first thing to check/calculate sunrise for the day
+    //q: will this need to be calculated everytime? 
+    //q: can I calculate withs with unix time?
+    //q: will time sync be needed? possibly handled via bluetooth app?
+    // do i want to use sunrise? civic dawn? 
+    // if were between sunrise and 45 minutes after sunrise
+  ////if(timestamp > sunrise and timestamp < sunrise + 45 minutes)
     // if it's within 5 minutes of sunrise 
+    //// if(timestamp < sunrise + 5minutes)
       // set output to low
    // else if its within 10 minutes 
+   ////else if (timetamp < sunrise + 10minutes)
       // set output to medium low
   // else if its within 15
+  ////else if (timetamp < sunrise + 15minutes)
     // set output to medium 
   // else if its within 20 
+  ////else if (timetamp < sunrise + 20minutes)
     // set output to medium high
   // else if its within in 25 minutes
+  ////else if (timetamp < sunrise + 25minutes)
     // set output to high
+    //otherwise lets flash or twirl or something to get your attention
+    ////else if (timetamp < sunrise + 30minutes)
 
+    
   // future idea: if it's withn 30 minutes and some type of
   // acknowlegment hasn't been met, set off audible alarm.
   
